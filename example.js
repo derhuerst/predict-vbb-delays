31312447025a91d4d7ee4b3166443afc56a843db
Jannis R <mail@jannisr.de>
2017-09-22 20:59:54 +0200

    separate stations whitelist, try different hidden layers count

--- example.js
+++ example.js
@@ -3,16 +3,33 @@
 const {NeuralNetwork} = require('brain.js')
 
 const depToFeatures = require('./lib/dep-to-features')
+const stations = require('./lib/stations')
 const data = require('./network.json')
 
 const network = new NeuralNetwork()
 network.fromJSON(data)
 
-const sample = depToFeatures({
+const transformResult = (result) => {
+	result.delay *= 3600
+	return result
+}
+
+const sample1 = depToFeatures({
 	station: {
 		id: '900000012151',
 		coordinates: {latitude: 52.500413, longitude: 13.38744}
 	},
-	when: '2017-09-22T14:20:30'
-})
-console.log(network.run(sample))
+	when: '2017-09-22T13:20:30'
+}, stations)
+console.log(sample1)
+console.log(transformResult(network.run(sample1)))
+
+const sample2 = depToFeatures({
+	station: {
+		id: '900000100513',
+		coordinates: {latitude: 52.516996, longitude: 13.388876}
+	},
+	when: '2017-09-22T13:20:30'
+}, stations)
+console.log(sample2)
+console.log(transformResult(network.run(sample2)))
--- index.js
+++ index.js
@@ -7,32 +7,23 @@ const floor = require('floordate')
 const brandenburg = require('german-states-bbox').BB
 
 const depToFeatures = require('./lib/dep-to-features')
+const stations = require('./lib/stations')
 
 const db = level(path.join(__dirname, 'vbb-delays.ldb'), {
 	valueEncoding: 'json'
 })
 const network = new NeuralNetwork({
-	hiddenLayers: [2]
+	hiddenLayers: [(stations.length + 9) * 2 / 3]
 })
 
 const clampDelay = delay => Math.min(1, delay / 3600)
-
-const stations = [
-	// '900000009102', // leo
-	// '900000100513', // unter den linden
-	'900000012151', // willy-brandt-haus
-	// '900000110015', // stahlheimer/wisbyer
-	// '900000160004', // lichtenberch
-	// '900000049241' // zehle
-]
 
 db.createValueStream()
 .on('error', console.error)
 .on('data', (dep) => {
 	if (!stations.includes(dep.station.id)) return
 
-	const input = depToFeatures(dep)
-	console.error(input)
+	const input = depToFeatures(dep, stations)
 	const output = {
 		delay: clampDelay(dep.delay)
 	}
@@ -40,7 +31,7 @@ db.createValueStream()
 	const res = network.train([
 		{input, output}
 	])
-	// console.error('-', res.error)
+	console.error(dep.station.id, '-', res.error)
 })
 .once('end', () => {
 	process.stdout.write(JSON.stringify(network.toJSON()) + '\n')
