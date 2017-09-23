ddcf2475a165bbd114ef59661a9eb8558a18cd4f
Jannis R <mail@jannisr.de>
2017-09-23 06:10:34 +0200

    example: network-file param

--- example.js
+++ example.js
@@ -1,14 +1,37 @@
 'use strict'
 
+const path = require('path')
+const level = require('level')
+const fs = require('fs')
 const {NeuralNetwork} = require('brain.js')
 const allStations = require('vbb-stations')
 
 const depToFeatures = require('./lib/dep-to-features')
 const stations = require('./lib/stations')
-const data = require('./network.json')
 
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+const argv = process.argv.slice(2)
+
+if (argv[0] === '-h' || argv[0] === '--help') {
+	process.stdout.write(`
+node train.js <network-file>\n`)
+}
+
+let networkFile = argv[0]
+if (!networkFile) showError('Invalid or missing network-file param.')
+networkFile = path.join(__dirname, networkFile)
+
+const networkData = JSON.parse(fs.readFileSync(networkFile, {encoding: 'utf8'}))
 const network = new NeuralNetwork()
-network.fromJSON(data)
+network.fromJSON(networkData)
+
+const db = level(path.join(__dirname, 'vbb-delays.ldb'), {
+	valueEncoding: 'json'
+})
 
 const transformResult = (result) => {
 	result.delay *= 3600
@@ -19,7 +42,7 @@ const runSample = (id, when) => {
 	const [station] = allStations(id)
 	if (!station) return
 
-	depToFeatures({station, when}, stations, (err, {input}) => {
+	depToFeatures({station, when}, stations, db, (err, {input}) => {
 		console.log(input)
 		console.log(transformResult(network.run(input)))
 	})
